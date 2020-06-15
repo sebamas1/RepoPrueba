8b60653a106cba08c7a7dc6b0fd2040e6535ec14
Sebastian <sebamas1@hotmail.com>
2020-06-15 02:13:38 -0300

    Mi primer commit

---
+++ src/main/java/Calculadora/Main.java
@@ -0,0 +1,15 @@
+package main.java.Calculadora;
+
+
+public class Main {
+String nombre;
+	public Main() {
+	
+}
+	private int sumar(int a, int b) {
+	return a+b;
+}
+private void cadena(String nombre) {
+	this.nombre = nombre;
+}
+}
---
+++ src/test/java/Calculadora/MainTest.java
@@ -0,0 +1,13 @@
+package test.java.Calculadora;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+	@Test
+	void test() {
+		//fail("Not yet implemented");
+	}
+
+}
